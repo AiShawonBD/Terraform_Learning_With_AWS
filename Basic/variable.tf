24c2d6483e30d95f45feac92f422cb143f0fc072
Ariful Islam Shawon <33293851+AiShawonBD@users.noreply.github.com>
2024-11-02 01:39:16 +0600

    User of Variable

--- Basic/variable.tf
+++ Basic/variable.tf
@@ -3,3 +3,9 @@
 variable "instance_type" {
   default = "t2.micro"
 }
+
+#User of Variable
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0866a3c8686eaeeba"  # Example AMI ID
+  instance_type = var.instance_type
+}
