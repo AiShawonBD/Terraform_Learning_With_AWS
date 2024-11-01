d801e9083fe6262a774c290e175bd45bfa1f2f63
Ariful Islam Shawon <33293851+AiShawonBD@users.noreply.github.com>
2024-11-01 16:37:59 +0600

    10.Example output blocks

--- Terraform_Project_02_VPC_Subnet_and_Apache_Web_Server/main.tf
+++ Terraform_Project_02_VPC_Subnet_and_Apache_Web_Server/main.tf
@@ -141,7 +141,7 @@ resource "aws_instance" "my_instance" {
 }
 
 
-# Example output blocks
+#10.Example output blocks 
 output "instance_public_ip" {
   value = aws_instance.my_instance.public_ip
   description = "The public IP address of the EC2 instance"
