dbca3f8999c951eda555d2ac5e5fa06edc65ed37
Ariful Islam Shawon <33293851+AiShawonBD@users.noreply.github.com>
2024-11-02 01:46:16 +0600

    Change the Variable Value in Different Ways

--- Basic/variable.tf
+++ Basic/variable.tf
@@ -10,3 +10,10 @@ resource "aws_instance" "my_instance" {
 }
 
 #Change the Variable Value in Different Ways
+/* Create a file in your project folder called terraform.tfvars.
+Inside this file, add the variable and set its value. For example: 
+
+instance_type = "t3.micro"
+
+Now, whenever you run terraform apply, Terraform will automatically pick up the value from terraform.tfvars.
+This is the easiest way to change variable values in one place without touching the main code. */
