f0dac3ffd0bd80b7c8234a5a0a6a9976778f26a9
Ariful Islam Shawon <33293851+AiShawonBD@users.noreply.github.com>
2024-11-02 01:42:16 +0600

    Change the Variable Value in Different Ways

--- Basic/variable.tf
+++ Basic/variable.tf
@@ -1,11 +1,12 @@
-#Variable Declear
-
+#Create Your First Variable
 variable "instance_type" {
   default = "t2.micro"
 }
 
-#User of Variable
+#Use the Variable in a Resource
 resource "aws_instance" "my_instance" {
   ami           = "ami-0866a3c8686eaeeba"  # Example AMI ID
   instance_type = var.instance_type
 }
+
+#Change the Variable Value in Different Ways
